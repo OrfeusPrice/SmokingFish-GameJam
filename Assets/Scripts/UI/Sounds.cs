415c3ebc439f87751130520c987e29a5b1006fea
Анна Петросян <apetrosia@sfedu.ru>
2024-06-30 16:08:05 +0300

    Add aound button

--- Assets/Scripts/UI/Sounds.cs
+++ Assets/Scripts/UI/Sounds.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Sounds : MonoBehaviour
 {
     [SerializeField] private AudioClip[] STs;
+    [SerializeField] private AudioClip[] sounds;
     [SerializeField] private int playNow;
+    [SerializeField] private Sprite[] soundIcons;
+    private UnityEngine.UI.Image button;
     private AudioSource source;
+    private float volume;
 
     void Start()
     {
+        volume = 1f;
         source = GetComponent<AudioSource>();
+        button = GameObject.Find("SoundButton").GetComponent<UnityEngine.UI.Image>();
         source.clip = STs[playNow];
         source.Play();
     }
@@ -18,5 +26,25 @@ public class Sounds : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlaySound(float vol, int index)
+    {
+        source.PlayOneShot(sounds[index], vol * volume);
+    }
+
+    public void SoundOnOff()
+    {
+        if (volume == 1)
+        {
+            volume = 0f;
+            button.sprite = soundIcons[0];
+        }
+        else
+        {
+            volume = 1f;
+            button.sprite = soundIcons[1];
+        }
+        source.volume = volume;
     }
 }
